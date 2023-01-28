05268d976ed547065a51fcaeb19047aeadf098ba
budzix-dev <pbudziak.github@gmail.com>
2023-01-28 17:49:25 +0100

    Contact Form Complete

--- components/contact/PhoneContactForm.vue
+++ components/contact/PhoneContactForm.vue
@@ -1,14 +1,15 @@
-<script setup>
+<script setup lang="ts">
 import api from "~~/helpers/shopApi";
 
 const defaultError = "Wystąpił błąd. Spróbuj ponownie później";
 let phoneInput = "";
 
-const loading = useState("loading", () => false);
-const errorMessage = useState("errorMessage", () => "");
+const loading = useState(() => false);
+const errorMessage = useState(() => "");
 const phone = usePhone();
+const cart = useCart();
 
-const handleSubmit = async (e) => {
+const handleSubmit = async (e: Event) => {
   e.preventDefault();
   loading.value = true;
   const params = {
@@ -18,8 +19,9 @@ const handleSubmit = async (e) => {
   try {
     const res = await api.post("/api/new_order", params);
     if (res.status !== 201) throw new Error();
+    cart.value = res.data.token;
     phone.value = phoneInput;
-  } catch (err) {
+  } catch (err: any) {
     errorMessage.value = err?.response?.data?.error_message || defaultError;
   } finally {
     loading.value = false;
@@ -31,7 +33,7 @@ const handleSubmit = async (e) => {
   <div
     class="w-screen max-w-sm md:max-w-md lg:max-w-2xl xl:max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8"
   >
-    <form class="space-y-6">
+    <form class="space-y-6" @submit="handleSubmit">
       <h5 class="text-xl xl:text-2xl font-medium text-gray-900">
         Skontaktuj się telefonicznie z konsultantem
       </h5>
@@ -58,7 +60,7 @@ const handleSubmit = async (e) => {
       <button
         class="w-full text-white bg-cyan-400 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm lg:text-lg px-5 py-2.5 text-center"
         :disabled="loading"
-        @click="handleSubmit"
+        type="submit"
       >
         Wyślij
       </button>
--- composables/states.ts
+++ composables/states.ts
@@ -1,1 +1,2 @@
 export const usePhone = () => useState<string | null>("phone", () => null);
+export const useCart = () => useState("cart", () => "");
