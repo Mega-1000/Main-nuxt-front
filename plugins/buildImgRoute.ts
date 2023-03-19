258b6c4afe967d23ebb5efbc03be946dded45aab
budzix-dev <pbudziak.github@gmail.com>
2023-03-19 21:47:18 +0100

    updated buildImgRoute

--- plugins/buildImgRoute.ts
+++ plugins/buildImgRoute.ts
@@ -1,9 +1,11 @@
+import { defaultImgSrc } from "~~/helpers/buildImgRoute";
+
 export default defineNuxtPlugin(() => {
   const baseUrl = useRuntimeConfig().public.baseUrl;
 
   return {
     provide: {
-      buildImgRoute: (path: string) => baseUrl + path,
+      buildImgRoute: (path: string) => (path ? baseUrl + path : defaultImgSrc),
     },
   };
 });
