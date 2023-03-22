ebb7d4014a690fc4e69dc29aebb742e8be5f99b7
Antoni Seba <antoniwoj@o2.pl>
2023-03-22 21:13:51 +0100

    Extract AccountActionButton to separate component

--- components/account/OrderItem.vue
+++ components/account/OrderItem.vue
@@ -107,9 +107,7 @@ const handleUploadProofOfPayment = async () => {
 </script>
 
 <template>
-  <div
-    class="relative space-y-3 rounded-xl shadow-lg p-3 w-[60vw] max-w-7xl mx-auto border border-white bg-white"
-  >
+  <div class="relative space-y-3 rounded-xl shadow-lg p-3 w-[60vw] max-w-7xl mx-auto border border-white bg-white">
     <p class="text-md">
       <span class="font-bold">Nr zamówienia:</span> {{ item.id }}
     </p>
@@ -140,10 +138,7 @@ const handleUploadProofOfPayment = async () => {
     <hr />
 
     <div class="relative overflow-x-auto">
-      <table
-        class="max-w-4xl text-sm text-left text-gray-500 w-full"
-        v-if="item.packages && item.packages.length > 0"
-      >
+      <table class="max-w-4xl text-sm text-left text-gray-500 w-full" v-if="item.packages && item.packages.length > 0">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50">
           <tr>
             <th scope="col" class="px-6 py-3">Nr paczki</th>
@@ -177,100 +172,69 @@ const handleUploadProofOfPayment = async () => {
 
     <div class="grid md:flex">
       <template v-if="item?.order_offers && item.order_offers.length > 0">
-        <a
-          is="button"
-          :href="`${config.baseUrl}/order-proform-pdf/${item.order_offers[0]?.id}`"
-          class="p-1 bg-slate-100 text-xs text-gray-900 border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700"
-        >
+        <a is="button" :href="`${config.baseUrl}/order-proform-pdf/${item.order_offers[0]?.id}`"
+          class="p-1 bg-slate-100 text-xs text-gray-900 border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700">
           Faktura proforma
         </a>
-        <a
-          is="button"
-          target="_blank"
-          :href="`${config.baseUrl}/order-offer-pdf/${item.order_offers[0]?.id}`"
-          class="p-1 bg-slate-100 text-xs text-gray-900 border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700"
-        >
+        <a is="button" target="_blank" :href="`${config.baseUrl}/order-offer-pdf/${item.order_offers[0]?.id}`"
+          class="p-1 bg-slate-100 text-xs text-gray-900 border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700">
           Opis oferty
         </a>
       </template>
-      <button
-        type="button"
-        @click="() => editCart(item.items)"
-        class="p-1 bg-slate-100 text-xs text-gray-900 border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700"
-      >
+
+      <accountActionButton :target="undefined" href="" type="button" @click="editCart(item.items)">
         Edytuj koszyk
-      </button>
-      <a
-        is="button"
-        :href="`${config.nuxtNewFront}zamowienie/mozliwe-do-realizacji/brak-danych/${item.id}`"
-        class="text-center p-1 bg-slate-100 text-xs text-gray-900 border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700"
-      >
+      </accountActionButton>
+
+      <accountActionButton :target="undefined" type="link" is="button"
+        :href="`${config.nuxtNewFront}zamowienie/mozliwe-do-realizacji/brak-danych/${item.id}`">
         Dane do dostawy i faktury oraz ich edycja
-      </a>
-      <a
-        v-for="invoice in item?.invoices?.filter((invoice: any) => invoice.is_visible_for_client)"
-        target="_blank"
-        is="button"
-        :href="`${config.baseUrl}/storage/invoices/${invoice.invoice_name}`"
-        class="p-1 bg-slate-100 text-xs text-gray-900 border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700"
-      >
+      </accountActionButton>
+
+      <accountActionButton v-for="invoice in item?.invoices?.filter((invoice: any) => invoice.is_visible_for_client)"
+        target="_blank" is="button" :href="`${config.baseUrl}/storage/invoices/${invoice.invoice_name}`" type="link">
         Pobierz fakturę {{ invoice.invoice_name }}
-      </a>
-      <button
-        v-for="invoice in item?.user_invoices"
-        @click="() => downloadInvoice(invoice)"
-        class="p-1 bg-slate-100 text-xs text-gray-900 border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700"
-      >
+      </accountActionButton>
+
+      <accountActionButton type="link"
+        v-for="invoice in item?.invoices?.filter((invoice: any) => invoice.is_visible_for_client)" target="_blank"
+        is="button" :href="`${config.baseUrl}/storage/invoices/${invoice.invoice_name}`">
+        Pobierz fakturę {{ invoice.invoice_name }}
+      </accountActionButton>
+
+      <button v-for="invoice in item?.user_invoices" @click="() => downloadInvoice(invoice)"
+        class="p-1 bg-slate-100 text-xs text-gray-900 border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700">
         Faktura: {{ invoice.gt_invoice_number }}
       </button>
-      <a
-        target="_blank"
-        is="button"
-        :href="`${config.baseUrl}/chat-show-or-new/${item.id}/${item.customer_id}`"
-        class="p-1 text-center bg-slate-100 text-xs text-gray-900 border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700"
-      >
+
+      <accountActionButton type="link" target="_blank" is="button"
+        :href="`${config.baseUrl}/chat-show-or-new/${item.id}/${item.customer_id}`">
         Czat
-      </a>
-      <button
-        @click="modal?.show"
-        v-if="!proofUploaded && (!item?.files || item.files.length === 0)"
-        class="p-1 bg-slate-100 text-xs text-gray-900 border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700"
-      >
+      </accountActionButton>
+
+      <accountActionButton type="button" :target="undefined" href="" @click="modal?.show"
+        v-if="!proofUploaded && (!item?.files || item.files.length === 0)">
         Podłącz potwierdzenie przelewu - przyśpiesza realizacje
-      </button>
-      <button
-        v-else
-        disabled
-        class="p-1 bg-green-400 text-xs text-black border border-gray-200"
-      >
+      </accountActionButton>
+
+      <accountActionButton type="button" :target="undefined" href="" v-else disabled
+        class="p-1 bg-green-400 text-xs text-black border border-gray-200">
         Potwierdzenie przelewu podłączone - zapłacono
-      </button>
+      </accountActionButton>
     </div>
-    <div
-      class="grid md:flex pt-2"
-      v-for="buttonGroup in Object.keys(item.buttons)"
-    >
+    <div class="grid md:flex pt-2" v-for="buttonGroup in Object.keys(item.buttons)">
       <p class="text-sm text-center pr-2 pt-1">{{ buttonGroup }}</p>
-      <a
-        v-for="button in (Object.values(item.buttons[buttonGroup]) as any)"
-        target="_blank"
-        is="button"
+      <a v-for="button in (Object.values(item.buttons[buttonGroup]) as any)" target="_blank" is="button"
         :href="button.url"
-        class="p-1 text-center bg-slate-100 text-xs text-gray-900 border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700"
-      >
+        class="p-1 text-center bg-slate-100 text-xs text-gray-900 border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700">
         {{ button.description }}
       </a>
     </div>
   </div>
 
-  <div
-    :id="`modal-${item.id}`"
-    tabindex="-1"
-    class="top-0 fixed z-50 w-auto hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full"
-  >
-    <div
-      class="relative w-full h-full max-w-xl sm:max-w-3xl md:max-w-5xl lg:max-w-7xl md:h-auto"
-    >
+  <div :id="`modal-${item.id}`" tabindex="-1"
+    class="top-0 fixed z-50 w-auto hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+    <div class="relative w-full h-full max-w-xl sm:max-w-3xl md:max-w-5xl lg:max-w-7xl md:h-auto">
       <!-- Modal content -->
       <div class="relative bg-white rounded-lg shadow">
         <!-- Modal header -->
@@ -278,47 +242,30 @@ const handleUploadProofOfPayment = async () => {
           <h3 class="text-xl font-semibold text-gray-900">
             Podłącz potwierdzenie przelewu
           </h3>
-          <button
-            type="button"
+          <button type="button"
             class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
-            data-modal-hide="modal"
-            @click="modal?.hide"
-          >
-            <svg
-              aria-hidden="true"
-              class="w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
+            data-modal-hide="modal" @click="modal?.hide">
+            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd"
                 d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
+                clip-rule="evenodd"></path>
             </svg>
             <span class="sr-only">Close modal</span>
           </button>
         </div>
         <!-- Modal body -->
         <div class="p-6 space-y-6 w-auto">
-          <template
-            v-if="
-              !item.dates?.customer_shipment_date_from ||
-              !item.dates?.customer_shipment_date_to
-            "
-          >
-            <div
-              class="flex max-w-lg p-6 bg-gray-100 border border-gray-200 rounded-lg shadow"
-            >
+          <template v-if="
+            !item.dates?.customer_shipment_date_from ||
+            !item.dates?.customer_shipment_date_to
+          ">
+            <div class="flex max-w-lg p-6 bg-gray-100 border border-gray-200 rounded-lg shadow">
               <h5 class="mb-2 text-lg font-md tracking-tight text-red-500 mr-6">
                 Uwaga brak danych do dostawy bądź faktury
               </h5>
-              <a
-                is="button"
-                :href="`${config.nuxtNewFront}zamowienie/mozliwe-do-realizacji/brak-danych/${item.id}`"
-                class="text-center p-1 bg-slate-100 text-md rounded-lg text-gray-900 border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700"
-              >
+              <a is="button" :href="`${config.nuxtNewFront}zamowienie/mozliwe-do-realizacji/brak-danych/${item.id}`"
+                class="text-center p-1 bg-slate-100 text-md rounded-lg text-gray-900 border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700">
                 Uzupełnij dane
               </a>
             </div>
@@ -342,14 +289,9 @@ const handleUploadProofOfPayment = async () => {
           <input type="file" id="proof_of_payment" accept=".pdf,image/*" />
         </div>
         <!-- Modal footer -->
-        <div
-          class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b"
-        >
-          <button
-            type="button"
-            @click="async () => await handleUploadProofOfPayment()"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-          >
+        <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b">
+          <button type="button" @click="async () => await handleUploadProofOfPayment()"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
             Jestem pewny poprawności oferty, podłączam potwierdzenie przelewu
           </button>
         </div>
