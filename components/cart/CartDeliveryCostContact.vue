84f6b6311ffa9bbf96552b17d847e2220cc285c7
Antoni Seba <antoniwoj@o2.pl>
2023-03-23 18:13:15 +0100

    Change text in cart delivery cost contact button

--- components/cart/CartDeliveryCostContact.vue
+++ components/cart/CartDeliveryCostContact.vue
@@ -1,25 +1,22 @@
 <script setup lang="ts">
-  const props = defineProps<{
-    disabled: boolean
-  }>()
+const props = defineProps<{
+  disabled: boolean
+}>()
 
-  const emit = defineEmits<{
-    (e: 'createChat'): void
-  }>()
+const emit = defineEmits<{
+  (e: 'createChat'): void
+}>()
 </script>
 
 <template>
   <div class="text-right">
     <button class="bg-blue-600 rounded px-4 py-2 text-white w-fit disabled:bg-slate-400" :disabled="disabled">
       <div>
-        Połącz mnie z konsultantem i potwierdz koszty transportu
-      </div>
-      <div>
-        Odpowiedź natychmistowa 7 - 23 poza następnego dnia 7 dni w tygodniu
-      </div>
-      <div>
-        Wymagane uzupełnienie danych kontaktowych poniżej
+        Połącz mnie z konsultantem aby potwierdzić koszty transportu
       </div>
     </button>
+    <div>
+      Poza tymi godzinami zostanie wysłane zapytanie ofertowe na które odpowiemy następnego dnia.
+    </div>
   </div>
 </template>
