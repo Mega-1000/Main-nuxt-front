474c897b6fe7aa60d4f97a38ef8acc05c2d642e3
Antoni Seba <antoniwoj@o2.pl>
2023-03-29 22:39:45 +0200

    minor changes

--- components/cart/CartDeliveryCostContact.vue
+++ components/cart/CartDeliveryCostContact.vue
@@ -10,13 +10,20 @@ const emit = defineEmits<{
 
 <template>
   <div class="text-right">
+    <div>
+      Przed uzyciem buttona koniecznie uzupełnij dane adresowe ponizej.
+    </div>
     <button class="bg-blue-600 rounded px-4 py-2 text-white w-fit disabled:bg-slate-400" :disabled="disabled">
       <div>
-        Połącz mnie z konsultantem aby potwierdzić koszty transportu
+        Połącz mnie z konsultantem aby potwierdzić koszty transportu godzina 7 - 23
       </div>
     </button>
     <div>
       Poza tymi godzinami zostanie wysłane zapytanie ofertowe na które odpowiemy następnego dnia.
     </div>
+
+    <button class="bg-blue-600 rounded px-4 py-2 text-white w-fit disabled:bg-slate-400" :disabled="disabled" @click="">
+      Wyślij do sprawdzenia kosztów transportu bez łączenia z konsultantem
+    </button>
   </div>
 </template>
