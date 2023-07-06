76042345f2a770857b1c8d6041ee28c8bf5921f0
Antoni Seba <antoniwoj@o2.pl>
2023-07-06 18:34:01 +0200

    Add basic newspaper outline

--- components/SubmitButton.vue
+++ components/SubmitButton.vue
@@ -6,7 +6,7 @@
 </script>
 
 <template>
-  <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" :disabled="disabled">
+  <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-gray-500" :disabled="disabled">
     <slot />
   </button>
 </template>
