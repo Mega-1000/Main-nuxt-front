642e7f5fb75d68e0c3f3ecd9176b2edc9600dff7
Antoni Seba <antoniwoj@o2.pl>
2023-08-31 10:06:28 +0200

    Fix cart edition

--- pages/Koszyk.html.vue
+++ pages/Koszyk.html.vue
@@ -121,7 +121,8 @@ const getPackagesNumber = async (cart: Cart) => {
 onMounted(async () => {
   const cart = new Cart();
   cart.init();
-  if (query?.cart_token) await prepareCartEdition(cart, query?.cart_token);
+
+  if (query?.cart_token && !cart.getEditedCart()) await prepareCartEdition(cart, query?.cart_token);
   await getPackagesNumber(cart);
 
   productsCart.value = cart;
