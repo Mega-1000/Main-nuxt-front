c6be8d9b9fb4be8fec9e736a40e879d3a8e0a6c6
Antoni Seba <antoniwoj@o2.pl>
2023-09-17 19:44:39 +0200

    Fix typo

--- pages/[productRewrite]/[productId].vue
+++ pages/[productRewrite]/[productId].vue
@@ -1,4 +1,4 @@
-`<script setup lang="ts">
+<script setup lang="ts">
 import findActiveMenu from "~~/helpers/findActiveMenu";
 import { defaultImgSrc } from "~~/helpers/buildImgRoute";
 import { Modal, ModalOptions } from "flowbite";
