d07a02e6740e6eb58c5469ce984af5407c8e3f1f
Antoni Seba <antoniwoj@o2.pl>
2023-10-20 14:53:51 +0200

    Add text to complaint

--- pages/forms.vue
+++ pages/forms.vue
@@ -5,6 +5,8 @@ onMounted(() => {
   const formName = route.params.formName;
   const orderId = route.params.orderId;
 
-  window.location.href = `https://admin.mega1000.pl/form/${formName}/${orderId}`;
+  setTimeout(() => {
+    window.location.href = `https://admin.mega1000.pl/form/${formName}/${orderId}`;
+  }, 10)
 })
 </script>
