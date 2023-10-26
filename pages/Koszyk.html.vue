ecea01bd61fe855f7ca6fa27bf3a85030100f775
Antoni Seba <antoniwoj@o2.pl>
2023-10-26 13:04:57 +0200

    Try to fix

--- pages/Koszyk.html.vue
+++ pages/Koszyk.html.vue
@@ -125,11 +125,12 @@ const getPackagesNumber = async (cart: Cart) => {
 onMounted(async () => {
   const cart = new Cart();
   cart.init();
+  await prepareCartEdition(cart, query?.cart_token)
 
-  if (query?.cart_token && !cart.getEditedCart() || query?.reloadCart) await prepareCartEdition(cart, query?.cart_token);
-  await getPackagesNumber(cart);
-
-  productsCart.value = cart;
+  // if (query?.cart_token && !cart.getEditedCart() || query?.reloadCart) await prepareCartEdition(cart, query?.cart_token);
+  // await getPackagesNumber(cart);
+  //
+  // productsCart.value = cart;
 });
 
 const handleDelete = async () => {
