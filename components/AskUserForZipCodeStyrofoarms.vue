841a88abfd7af223ebaa4d7bc376225a82478bab
Antoni Seba <antoniwoj@o2.pl>
2024-02-09 20:12:58 +0200

    Add zip code ask for user

--- components/AskUserForZipCodeStyrofoarms.vue
+++ components/AskUserForZipCodeStyrofoarms.vue
@@ -11,8 +11,8 @@
 </script>
 
 <template>
-  <div class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-2xl max-h-full">
+  <div class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full" style="background-color: rgba(0, 0, 0, 0.17)">
+    <div class="relative p-4 w-full max-w-2xl max-h-full mx-auto">
       <!-- Modal content -->
       <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
         <!-- Modal header -->
