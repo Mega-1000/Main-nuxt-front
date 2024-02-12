702c3227d9c3aa51efe99f660bc94129a2c64e74
Antoni Seba <antoniwoj@o2.pl>
2024-02-12 09:47:18 +0200

    Add zip code ask for user

--- pages/firms/Przetargi.vue
+++ pages/firms/Przetargi.vue
@@ -37,84 +37,86 @@ const showOfferTable = (auction: any) => {
 </script>
 
 <template>
-  <SubmitButton>
-    <a :href="`https://new.mega1000.pl/magazyn/aktualizacja-cen/${currentFirm.id}/zaktualizuj`">
-      Zaktualizuj ceny produktów przydzielonych do twojego konta
-    </a>
-  </SubmitButton>
+  <div class="mx-auto w-1/2">
+    <SubmitButton>
+      <a :href="`https://new.mega1000.pl/magazyn/aktualizacja-cen/${currentFirm.id}/zaktualizuj`" target="__blank" class="mt-4">
+        Zaktualizuj ceny produktów przydzielonych do twojego konta
+      </a>
+    </SubmitButton>
 
-  <div v-for="auction in auctions" class="border mx-auto w-1/2 p-3 mt-12 text-lg">
-    <div class=" flex justify-between">
-      <div>
-        Numer przetargu: {{ auction.id }}
-        <br>Koniec przetargu {{ auction.end_of_auction }}
-        <br>Wstępna data dostawy wskazana przez klienta {{ auction.date_of_delivery }}
-        <br>Procentowy udział ceny {{ auction.price }}
-        <br>Procentowy udział jakości {{ auction.quality }}
-        <br>Data rozpoczęcia przetargu {{ auction.created_at }}
-        <br>Zaktualizowana {{ auction.updated_at }}
-        <br>Email klienta: {{ auction.chat.order.customer.login }}
-        <br>Telefon klienta: {{ auction.chat.order.customer.addresses[0].phone }} <a :href="`tel:${auction.chat.order.customer.addresses[0].phone}`">Zadzwoń</a>
+    <div v-for="auction in auctions" class="border p-3 mt-12 text-lg">
+      <div class=" flex justify-between">
+        <div>
+          Numer przetargu: {{ auction.id }}
+          <br>Koniec przetargu {{ auction.end_of_auction }}
+          <br>Wstępna data dostawy wskazana przez klienta {{ auction.date_of_delivery }}
+          <br>Procentowy udział ceny {{ auction.price }}
+          <br>Procentowy udział jakości {{ auction.quality }}
+          <br>Data rozpoczęcia przetargu {{ auction.created_at }}
+          <br>Zaktualizowana {{ auction.updated_at }}
+          <br>Email klienta: {{ auction.chat.order.customer.login }}
+          <br>Telefon klienta: {{ auction.chat.order.customer.addresses[0].phone }} <a :href="`tel:${auction.chat.order.customer.addresses[0].phone}`">Zadzwoń</a>
+        </div>
+
+        <div>
+          <a :href="auction.editPricesLink" target="__blank">
+            <SubmitButton>
+              Zmień swoje ceny w tym przetargu
+            </SubmitButton>
+          </a>
+
+          <br>
+
+  <!--        Oferty:-->
+  <!--        <div v-for="offer in auction.activeOffers" class="tooltip">-->
+  <!--            ID: {{ offer.id }} Firma: {{ offer.firm.symbol }} Cena: {{ offer.basic_price_net }}-->
+  <!--            <span class="tooltip-text">-->
+  <!--              Cena za opakowanie handlowe: {{ offer.commercial_price_net }}<br>-->
+  <!--              Cena za m3: {{ offer.basic_price_net }}<br>-->
+  <!--              cena za m2: {{ offer.calculated_price_net }}<br>-->
+  <!--            </span>-->
+  <!--        </div>-->
+
+  <!--        <SubmitButton @click="auction.offersExpanded === false ? expandOffers(auction) : collapseOffers(auction)">-->
+  <!--          <span v-if="auction.offersExpanded === false">-->
+  <!--            Wyświetl wszystkie oferty-->
+  <!--          </span>-->
+  <!--          <span v-else>-->
+  <!--            Ogranicz oferty-->
+  <!--          </span>-->
+  <!--        </SubmitButton>-->
+
+          <button @click="showOfferTable(auction)" class="bg-green-500 mt-4 block px-4 py-2 rounded text-sm">
+            Zobacz tabelę ofert
+          </button>
+        </div>
       </div>
 
-      <div>
-        <a :href="auction.editPricesLink" target="__blank">
+      <div v-if="auction.offerTableShown" class="mt-4">
+        <table>
+          <tr>
+            <th>Produkt</th>
+            <th>Ilość opakowań</th>
+            <th>Ilość M3</th>
+            <th>Twoja cena</th>
+            <th>Najniższa cena na ten moment</th>
+          </tr>
+
+          <tr v-for="item in auction.chat.order.items" class="mt-4">
+            <td>{{ item.product.name.substr(item.product.name.indexOf(" ") + 1) }}</td>
+            <td>{{ item.quantity }}</td>
+            <td>{{  Math.round(item.quantity * item.product.packing.numbers_of_basic_commercial_units_in_pack * 100) / 100 }} {{ item.product.unit_basic }}</td>
+            <td>{{ auction.offers.filter((offer) => offer.firm_id === currentFirm.id).sort((a, b) => a.basic_price_net - b.basic_price_net)[0]?.basic_price_net }}</td>
+            <td>{{ auction.offers.filter((offer) => offer.order_item_id === item.id).sort((a, b) => a.basic_price_net - b.basic_price_net)[0]?.basic_price_net }}</td>
+          </tr>
+        </table>
+
+        <span>
           <SubmitButton>
-            Zmień swoje ceny w tym przetargu
+            Zmień swoje ceny w tej ofercie
           </SubmitButton>
-        </a>
-
-        <br>
-
-<!--        Oferty:-->
-<!--        <div v-for="offer in auction.activeOffers" class="tooltip">-->
-<!--            ID: {{ offer.id }} Firma: {{ offer.firm.symbol }} Cena: {{ offer.basic_price_net }}-->
-<!--            <span class="tooltip-text">-->
-<!--              Cena za opakowanie handlowe: {{ offer.commercial_price_net }}<br>-->
-<!--              Cena za m3: {{ offer.basic_price_net }}<br>-->
-<!--              cena za m2: {{ offer.calculated_price_net }}<br>-->
-<!--            </span>-->
-<!--        </div>-->
-
-<!--        <SubmitButton @click="auction.offersExpanded === false ? expandOffers(auction) : collapseOffers(auction)">-->
-<!--          <span v-if="auction.offersExpanded === false">-->
-<!--            Wyświetl wszystkie oferty-->
-<!--          </span>-->
-<!--          <span v-else>-->
-<!--            Ogranicz oferty-->
-<!--          </span>-->
-<!--        </SubmitButton>-->
-
-        <button @click="showOfferTable(auction)" class="bg-green-500 mt-4 block px-4 py-2 rounded text-sm">
-          Zobacz tabelę ofert
-        </button>
+        </span>
       </div>
-    </div>
-
-    <div v-if="auction.offerTableShown" class="mt-4">
-      <table>
-        <tr>
-          <th>Produkt</th>
-          <th>Ilość opakowań</th>
-          <th>Ilość M3</th>
-          <th>Twoja cena</th>
-          <th>Najniższa cena na ten moment</th>
-        </tr>
-
-        <tr v-for="item in auction.chat.order.items" class="mt-4">
-          <td>{{ item.product.name.substr(item.product.name.indexOf(" ") + 1) }}</td>
-          <td>{{ item.quantity }}</td>
-          <td>{{  Math.round(item.quantity * item.product.packing.numbers_of_basic_commercial_units_in_pack * 100) / 100 }} {{ item.product.unit_basic }}</td>
-          <td>{{ auction.offers.filter((offer) => offer.firm_id === currentFirm.id).sort((a, b) => a.basic_price_net - b.basic_price_net)[0]?.basic_price_net }}</td>
-          <td>{{ auction.offers.filter((offer) => offer.order_item_id === item.id).sort((a, b) => a.basic_price_net - b.basic_price_net)[0]?.basic_price_net }}</td>
-        </tr>
-      </table>
-
-      <span>
-        <SubmitButton>
-          Zmień swoje ceny w tej ofercie
-        </SubmitButton>
-      </span>
     </div>
   </div>
 </template>
