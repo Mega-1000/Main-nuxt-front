b618dad1848700babe4183c5954bfae41a79fcd9
Antoni Seba <antoniwoj@o2.pl>
2024-02-16 18:04:33 +0100

    Add zip code ask for user

--- pages/przetargi-styropianów.vue
+++ pages/przetargi-styropianów.vue
@@ -1,27 +1,37 @@
 <script setup>
-  const { $shopApi: shopApi } = useNuxtApp();
+import { ref, onMounted } from 'vue';
+const { $shopApi: shopApi } = useNuxtApp();
 
-  const styrofoamTypes = ref([]);
-  const selectedStyrofoamType = ref('');
+const styrofoamTypes = ref([]);
+const selections = ref([{ value: null }]);
 
-  onMounted(async () => {
-    const types = await shopApi.get('/auctions/get-styrofoarm-types');
+onMounted(async () => {
+  const types = await shopApi.get('/auctions/get-styrofoam-types');
+  styrofoamTypes.value = Array.from(types.data.map((styrofoam) => ({ label: styrofoam, value: styrofoam })));
+});
 
-    styrofoamTypes.value = Array.from(types.data.map((styrofoam) => {return {'label': styrofoam, 'value': styrofoam}}));
-  });
+const addSelection = () => {
+  if (selections.value.length < 5) {
+    selections.value.push({ value: null });
+  }
+};
 
-  // watch(selectedStyrofoamType, selectedStyrofoamTypeUpdated())
-  //
-  // const selectedStyrofoamTypeUpdated = () => {
-  //
-  // }
+const updateSelection = (index, newValue) => {
+  selections.value[index].value = newValue;
+  if (index === selections.value.length - 1 && selections.value.length < 5) {
+    addSelection();
+  }
+};
 </script>
 
 <template>
-  <SelectInput
-      label="Wybierz rodzaj styropianu"
-      :options="styrofoamTypes"
-      v-model="selectedStyrofoamType"
-      v-if="styrofoamTypes.length !== 0"
-  />
+  <div v-for="(selection, index) in selections" :key="index">
+    <SelectInput
+        label="Wybierz rodzaj styropianu"
+        :options="styrofoamTypes"
+        v-model="selection.value"
+        @change="updateSelection(index, $event)"
+        v-if="styrofoamTypes.length !== 0"
+    />
+  </div>
 </template>
