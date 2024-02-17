58c708ab14d0d85d151dc1f6adff31eb90998f6b
Antoni Seba <antoniwoj@o2.pl>
2024-02-17 09:56:25 +0100

    Styrofoams ui advancements

--- pages/przetargi-styropianów.vue
+++ pages/przetargi-styropianów.vue
@@ -27,7 +27,7 @@ const updateSelection = (index, newValue) => {
 
 const showQuotes = async (name) => {
   const {data: request} = await shopApi.get(`/auctions/get-quotes-by-styrofoarm-type/${name.value}`);
-
+  console.log(name)
   modalData.value = request;
 }
 </script>
@@ -65,6 +65,13 @@ const showQuotes = async (name) => {
                 <h3 class="text-xl font-semibold text-gray-900">
                   Wycena dostępnych firm dla tego styropianu
                 </h3>
+
+                <button type="button" @click="modalData = false" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                  <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                  </svg>
+                  <span class="sr-only">Close modal</span>
+                </button>
               </div>
               <!-- Modal body -->
               <div class="p-4 md:p-5 space-y-4">
