88c4a6211aece1ce4d85df46473e55adcca640ac
Antoni Seba <antoniwoj@o2.pl>
2024-02-17 18:55:47 +0100

    Styrofoams ui advancements

--- components/NavbarShipmentCostTable.vue
+++ components/NavbarShipmentCostTable.vue
@@ -37,112 +37,112 @@
 
 <template>
   <div class="ml-4 fixed z-10 rounded-lg p-4 bg-gray-50">
-    <transition name="fade" mode="out-in">
-      <div v-if="showBox">
-        <div>
-          Kosty dostawy:
-        </div>
-
-        <table class="table-auto">
-          <thead>
-          <tr>
-            <th></th>
-            <th></th>
-            <th style="border-left: 2px solid black" colspan="2" class="text-center">
-              Dłużyca
-            </th>
-          </tr>
-          <tr>
-            <td>
-              Kurier
-            </td>
-
-            <td v-for="(v, k) in table">
-              {{ k }}
-            </td>
-          </tr>
-          </thead>
-
-          <tbody>
-            <tr>
-              <td>
-                Ilość paczek
-              </td>
-
-              <td>
-                {{ Math.round(table.GLS * 100) / 100 }}
-              </td>
-
-              <td>
-                {{ Math.round(table.GLs * 100) / 100 }}
-              </td>
-
-              <td>
-                {{ Math.round(table.DPD * 100) / 100 }}
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                po zaokrlągl.
-              </td>
-
-              <td>
-                {{ Math.ceil(table.GLS) }}
-              </td>
-
-              <td>
-                {{ Math.ceil(table.GLs) }}
-              </td>
-
-              <td>
-                {{ Math.ceil(table.DPD) }}
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                Cena w PLN
-              </td>
-
-              <td>
-                18
-              </td>
-
-              <td>
-                18
-              </td>
-
-              <td>
-                48
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                Warość w PLN
-              </td>
-
-              <td>
-                {{ Math.ceil(table.GLS) * 18 }}
-              </td>
-
-              <td>
-                {{ Math.ceil(table.GLs) * 18 }}
-              </td>
-
-              <td>
-                {{ Math.ceil(table.DPD) * 48 }}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-
-        <div class="mt-4">
-          <b>Suma:</b> {{ total }} zł
-        </div>
-      </div>
-    </transition>
+<!--    <transition name="fade" mode="out-in">-->
+<!--      <div v-if="showBox">-->
+<!--        <div>-->
+<!--          Kosty dostawy:-->
+<!--        </div>-->
+<!---->
+<!--        <table class="table-auto">-->
+<!--          <thead>-->
+<!--          <tr>-->
+<!--            <th></th>-->
+<!--            <th></th>-->
+<!--            <th style="border-left: 2px solid black" colspan="2" class="text-center">-->
+<!--              Dłużyca-->
+<!--            </th>-->
+<!--          </tr>-->
+<!--          <tr>-->
+<!--            <td>-->
+<!--              Kurier-->
+<!--            </td>-->
+<!---->
+<!--            <td v-for="(v, k) in table">-->
+<!--              {{ k }}-->
+<!--            </td>-->
+<!--          </tr>-->
+<!--          </thead>-->
+<!---->
+<!--          <tbody>-->
+<!--            <tr>-->
+<!--              <td>-->
+<!--                Ilość paczek-->
+<!--              </td>-->
+<!---->
+<!--              <td>-->
+<!--                {{ Math.round(table.GLS * 100) / 100 }}-->
+<!--              </td>-->
+<!---->
+<!--              <td>-->
+<!--                {{ Math.round(table.GLs * 100) / 100 }}-->
+<!--              </td>-->
+<!---->
+<!--              <td>-->
+<!--                {{ Math.round(table.DPD * 100) / 100 }}-->
+<!--              </td>-->
+<!--            </tr>-->
+<!---->
+<!--            <tr>-->
+<!--              <td>-->
+<!--                po zaokrlągl.-->
+<!--              </td>-->
+<!---->
+<!--              <td>-->
+<!--                {{ Math.ceil(table.GLS) }}-->
+<!--              </td>-->
+<!---->
+<!--              <td>-->
+<!--                {{ Math.ceil(table.GLs) }}-->
+<!--              </td>-->
+<!---->
+<!--              <td>-->
+<!--                {{ Math.ceil(table.DPD) }}-->
+<!--              </td>-->
+<!--            </tr>-->
+<!---->
+<!--            <tr>-->
+<!--              <td>-->
+<!--                Cena w PLN-->
+<!--              </td>-->
+<!---->
+<!--              <td>-->
+<!--                18-->
+<!--              </td>-->
+<!---->
+<!--              <td>-->
+<!--                18-->
+<!--              </td>-->
+<!---->
+<!--              <td>-->
+<!--                48-->
+<!--              </td>-->
+<!--            </tr>-->
+<!---->
+<!--            <tr>-->
+<!--              <td>-->
+<!--                Warość w PLN-->
+<!--              </td>-->
+<!---->
+<!--              <td>-->
+<!--                {{ Math.ceil(table.GLS) * 18 }}-->
+<!--              </td>-->
+<!---->
+<!--              <td>-->
+<!--                {{ Math.ceil(table.GLs) * 18 }}-->
+<!--              </td>-->
+<!---->
+<!--              <td>-->
+<!--                {{ Math.ceil(table.DPD) * 48 }}-->
+<!--              </td>-->
+<!--            </tr>-->
+<!--          </tbody>-->
+<!--        </table>-->
+<!---->
+<!--        <div class="mt-4">-->
+<!--          <b>Suma:</b> {{ total }} zł-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </transition>-->
 
     <button @click="showBox = !showBox">{{ showBox ? 'ZAMKNIJ TABELĘ' : 'Otwórz tabelę kosztów transportu' }}</button>
   </div>
