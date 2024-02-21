4e60626847452d456ad862b94c689b01d51efab7
Antoni Seba <antoniwoj@o2.pl>
2024-02-21 11:00:24 +0100

    Styrofoams ui advancements

--- pages/porady-do-zakupu-styropianow.vue
+++ pages/porady-do-zakupu-styropianow.vue
@@ -13,8 +13,8 @@ function onIframeLoad() {
 <template>
   <div style="position: relative; width: 70%; margin: 0 auto; height: 80vh;">
     <!-- Loader displayed while isLoading is true -->
-    <div v-if="isLoading" style="position: absolute; top: 0; left: 0; right: 0; bottom: 0; display: flex; justify-content: center; align-items: center; background-color: rgba(255, 255, 255, 0.8); z-index: 100;">
-      <span>Loading...</span>
+    <div v-if="isLoading" style="position: absolute; top: 0; left: 0; right: 0; bottom: 0; display: flex; justify-content: center; align-items: center; background-color: rgba(255, 255, 255, 0.8); z-index: 100; font-weight: bold; font-size: larger">
+      <span>Ładowanie tabeli, proszę czekać</span>
     </div>
 
     <!-- IFrame with the load event listener -->
