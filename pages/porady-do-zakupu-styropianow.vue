ee460855ee1244a1deee2b4c626d1a7d323b3455
Antoni Seba <antoniwoj@o2.pl>
2024-02-21 10:55:35 +0100

    Styrofoams ui advancements

--- pages/porady-do-zakupu-styropianow.vue
+++ pages/porady-do-zakupu-styropianow.vue
@@ -1,7 +1,23 @@
 <script setup>
+import { ref } from 'vue';
 
+// State to track if the iframe is loading
+const isLoading = ref(true);
+
+// Function to call when the iframe has finished loading
+function onIframeLoad() {
+  isLoading.value = false;
+}
 </script>
 
 <template>
-  <iframe src="https://admin.mega1000.pl/auctions/display-prices-table" style="width: 70%; margin: 0 auto; height: 80vh"></iframe>
+  <div style="position: relative; width: 70%; margin: 0 auto; height: 80vh;">
+    <!-- Loader displayed while isLoading is true -->
+    <div v-if="isLoading" style="position: absolute; top: 0; left: 0; right: 0; bottom: 0; display: flex; justify-content: center; align-items: center; background-color: rgba(255, 255, 255, 0.8); z-index: 100;">
+      <span>Loading...</span>
+    </div>
+
+    <!-- IFrame with the load event listener -->
+    <iframe src="https://admin.mega1000.pl/auctions/display-prices-table" style="width: 100%; height: 100%;" @load="onIframeLoad"></iframe>
+  </div>
 </template>
