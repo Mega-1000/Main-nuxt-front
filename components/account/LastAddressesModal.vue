4a3e595a051aef9f3861354b76274e3eb4b56367
Antoni Seba <antoniwoj@o2.pl>
2024-04-10 20:08:08 +0200

    Make recalc from csv work

--- components/account/LastAddressesModal.vue
+++ components/account/LastAddressesModal.vue
@@ -42,25 +42,22 @@ const submitOption = (id) => {
 };
 </script>
 
-
 <template>
-  <SubmitButton @click="modal?.show()">
+  <SubmitButton @click="showModal = true">
     Wyświetl dane z kilku ostatnich zamówień
   </SubmitButton>
 
-  <div :id="`modal`" tabindex="-1"
-       class="top-0 fixed z-50 w-auto hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-    <div class="relative w-full h-full max-w-xl sm:max-w-3xl md:max-w-5xl lg:max-w-7xl md:h-auto">
-      <!-- Modal content -->
+  <div class="fixed inset-0 z-50 flex items-center justify-center p-4 overflow-x-hidden overflow-y-auto bg-gray-900 bg-opacity-50"
+       v-if="showModal">
+    <div class="relative w-full max-w-xl sm:max-w-3xl md:max-w-5xl lg:max-w-7xl h-auto">
       <div class="relative bg-white rounded-lg shadow">
-        <!-- Modal header -->
         <div class="flex items-start justify-between p-4 border-b rounded-t">
           <h3 class="text-xl font-semibold text-gray-900">
             Wybierz adres
           </h3>
           <button type="button"
                   class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
-                  data-modal-hide="modal" @click="modal?.hide">
+                  @click="showModal = false">
             <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
                  xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd"
@@ -70,82 +67,76 @@ const submitOption = (id) => {
             <span class="sr-only">Zamknij modal</span>
           </button>
         </div>
-        <!-- Modal body -->
-        <div class="p-6 space-y-6 w-auto">
-
-          <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-              <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th scope="col" class="px-6 py-3">
-                  Numer oferty
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  Imię
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  Nazwisko
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  Ulica/wioska
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  Miasto
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  Kod pocztowy
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  email
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  telefon
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  akcje
-                </th>
-              </tr>
-              </thead>
-              <tbody>
-                <tr v-for="address in addresses" class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                  <td class="px-6 py-4">
-                    {{ address.adress?.order_id }}
-                  </td>
-                  <td class="px-6 py-4">
-
-                  </td>
-                  <td class="px-6 py-4">
-                    {{ address.adress?.firstname }}
-                  </td>
-                  <td class="px-6 py-4">
-                    {{ address.adress?.lastname }}
-                  </td>
-                  <td class="px-6 py-4">
-                    {{ address.adress?.address }}
-                  </td>
-                  <td class="px-6 py-4">
-                    {{ address.adress?.city }}
-                  </td>
-                  <td class="px-6 py-4">
-                    {{ address?.adress?.postal_code }}
-                  </td>
-                  <td class="px-6 py-4">
-                    {{ address.adress?.email }}
-                  </td>
-                  <td class="px-6 py-4">
-                    {{ address.adress?.phone }}
-                  </td>
-                  <td class="px-6 py-4">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="submitOption(address.id)">
-                      Wybierz
-                    </button>
-                  </td>
-                </tr>
+        <div class="p-4 overflow-x-auto">
+          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="px-6 py-3">
+                Numer oferty
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Imię
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Nazwisko
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Ulica/wioska
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Miasto
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Kod pocztowy
+              </th>
+              <th scope="col" class="px-6 py-3">
+                email
+              </th>
+              <th scope="col" class="px-6 py-3">
+                telefon
+              </th>
+              <th scope="col" class="px-6 py-3">
+                akcje
+              </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="address in addresses" class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+              <td class="px-6 py-4">
+                {{ address.adress?.order_id }}
+              </td>
+              <td class="px-6 py-4">
+                {{ address.adress?.firstname }}
+              </td>
+              <td class="px-6 py-4">
+                {{ address.adress?.lastname }}
+              </td>
+              <td class="px-6 py-4">
+                {{ address.adress?.address }}
+              </td>
+              <td class="px-6 py-4">
+                {{ address.adress?.city }}
+              </td>
+              <td class="px-6 py-4">
+                {{ address?.adress?.postal_code }}
+              </td>
+              <td class="px-6 py-4">
+                {{ address.adress?.email }}
+              </td>
+              <td class="px-6 py-4">
+                {{ address.adress?.phone }}
+              </td>
+              <td class="px-6 py-4">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        @click="submitOption(address.id)">
+                  Wybierz
+                </button>
+              </td>
+            </tr>
             </tbody>
           </table>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
