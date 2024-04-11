8dde7f98787002d939f35b9f8768f969c9730d62
Antoni Seba <antoniwoj@o2.pl>
2024-04-11 16:58:33 +0200

    Make recalc from csv work

--- pages/styropiany.vue
+++ pages/styropiany.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <header class="header py-6 shadow-md bg-white">
+    <header class="header py-6 shadow-md bg-gray-100">
       <div class="container mx-auto flex justify-between items-center">
         <a href="/" class="text-2xl font-bold text-gray-800">Eph Polska</a>
         <nav class="flex space-x-6">
@@ -22,11 +22,11 @@
         </div>
       </section>
 
-      <section class="py-20 px-4">
+      <section class="py-20 px-4 bg-white">
         <div class="container mx-auto">
           <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
             <div
-                class="feature-card bg-white rounded-lg shadow-lg p-8 flex flex-col justify-center items-center space-y-4 transition-transform duration-300 hover:-translate-y-2"
+                class="feature-card bg-gray-100 rounded-lg shadow-lg p-8 flex flex-col justify-center items-center space-y-4 transition-transform duration-300 hover:-translate-y-2"
             >
               <div class="bg-blue-500 rounded-full p-4 mb-4">
                 <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -43,7 +43,7 @@
               </p>
             </div>
             <div
-                class="feature-card bg-white rounded-lg shadow-lg p-8 flex flex-col justify-center items-center space-y-4 transition-transform duration-300 hover:-translate-y-2"
+                class="feature-card bg-gray-100 rounded-lg shadow-lg p-8 flex flex-col justify-center items-center space-y-4 transition-transform duration-300 hover:-translate-y-2"
             >
               <div class="bg-blue-500 rounded-full p-4 mb-4">
                 <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -60,7 +60,7 @@
               </p>
             </div>
             <div
-                class="feature-card bg-white rounded-lg shadow-lg p-8 flex flex-col justify-center items-center space-y-4 transition-transform duration-300 hover:-translate-y-2"
+                class="feature-card bg-gray-100 rounded-lg shadow-lg p-8 flex flex-col justify-center items-center space-y-4 transition-transform duration-300 hover:-translate-y-2"
             >
               <div class="bg-blue-500 rounded-full p-4 mb-4">
                 <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -81,13 +81,83 @@
       </section>
 
       <section class="py-20 px-4 bg-gray-100">
+        <div class="container mx-auto text-center">
+          <h2 class="text-4xl md:text-5xl font-bold mb-10">Jak dokonać zakupu? </h2>
+          <p class="text-lg md:text-xl mb-10 text-left">
+            Styropiany znajdują się w 3 działach <br>
+            160 - fundamentowy <br>
+            300 - posadzkowy <br>
+            320 - elewacyjny <br>
+            Przed zakupem warto przejrzeć sobie tabele wszystkich kilkudziesięciu producentów która znajduję się w każdym powyższych działów
+            <br>
+            Następnie za pomocą kalkulatorów znajdujących się przy każdym asortymencie w łatwy sposób można dokonać obliczeń potrzebnego styropianu w m2 m3 czy też w opakowaniach handlowych
+            <br>
+            Po dokonaniu obliczeń i wrzuceniu do koszyka odpowiedniego asortymentu na górze znajduje się ikona koszyka na którą trzeba kliknąć.
+            <br>
+            Po kliknięciu otrzymamy spis całości asortymentu który został dokonany w którym można jeszcze dokonać korekty pod względem ilościowym.
+            <br>
+            Na końcu prosimy uzupełnić niezbędne dane i wysłać nam do sprawdzenia poprawności. <br>
+            Bezzwłocznie po jego otrzymaniu sprawdzimy pod tym kątem po czym odeślemy informacje na e maila w przypadku gdy oferta zostanie już przygotowana.
+            <br>
+            Ofertę taką można będzie sprawdzić po zalogowaniu się na swoje konto na naszej stronie. <br>
+            Szczegóły transakcji omawiacie z przedstawicielem handlowym danej fabryki z którym kontakt będziecie mieć poprze chata lub telefon według uznania.
+            <br>
+            Istnieje możliwość płatności jako niewielka zaliczka i reszta płatność przy dostawie. <br>
+          </p>
+        </div>
+      </section>
+
+      <section class="py-20 px-4 bg-white">
+        <div class="container mx-auto text-center">
+          <h2 class="text-4xl md:text-5xl font-bold mb-10">Jakiego producenta wybrać?</h2>
+          <p class="text-lg md:text-xl mb-10 text-left">
+            W przypadku wyboru danego gatunku styropianu o identycznych danych technicznych, każda z fabryk może wyprodukować go pod względem jakości w różnym stopniu.
+            <br>
+            Niemal wszyscy sprzedawcy namawiają na zakup tego producenta, na którym mają największy zysk. <br>
+            Nasze podejście jest całkowicie odmienne i innowacyjne, ponieważ nie zarabiamy w ogóle na tym produkcie i jest nam całkowicie obojętne, którego producenta wybierzecie.
+            <br>
+            Zakup możecie dokonać bezpośrednio z fabryki lub przez przedstawiciela wskazanego przez fabrykę lub przez nasz system, wybór pozostawiamy Wam.
+            <br>
+            Samodzielny wybór jest bardzo trudny, którego producenta wybrać, ale poniżej podajemy na jakie opinie należy zwrócić uwagę:
+            <br>
+            - sprzedawców, którzy niemal zawsze sprzedają tych producentów, na których mają największy zysk, łatwo to zauważyć po tym, że mają u siebie tylko kilku wybranych, najbardziej dla siebie korzystnych producentów.
+            <br>
+            - opinie internetowe, które są bardzo często lobbowane i pisane na zlecenie firm produkcyjnych jako super zadowoleni klienci, czytanie ich wprowadza tylko w błąd.
+            <br>
+            - pseudoznawców, którzy nie mają wiedzy i doświadczenia lub mają korzyść z Państwa wyboru. <br>
+            Dlatego z naszej strony proponujemy, aby dowiedzieć się u kogoś mocno zaufanego, który nie będzie miał żadnej korzyści z Państwa wyboru, aczkolwiek będzie posiadał dużą wiedzę i doświadczenie.
+            <br>
+            Dla osób, które jednak chcą otrzymać jakąś informację na temat danego producenta, podajemy na samym dole nasze spostrzeżenia, ale prosimy pamiętać, że jest to nasza subiektywna opinia, i prosimy najlepiej skorzystać z opinii z innych zaufanych źródeł.
+            <br>
+          </p>
+        </div>
+      </section>
+
+      <section class="py-20 px-4 bg-gray-100">
+        <div class="container mx-auto text-center">
+          <h2 class="text-4xl md:text-5xl font-bold mb-10">Gwarancja najniższej ceny - system przetargowy zakupu styropianu</h2>
+          <p class="text-lg md:text-xl mb-10 text-left">
+            W przypadku zakupu dużych ilości powyżej 100m3 najlepszą formą zakupu jest tak zwany przetarg. <br>
+            W tym przypadku proponujemy wybrać z tabeli wstępnie ulubionego producenta po czym wrzucić do koszyka wybrany asortyment i wysłać do systemu.
+            <br>
+            Następnie zalogować się na swoje konto na naszej stronie po czym dokonać za pomocą przycisku ROZPOCZNIJ PRZETARG
+            <br>
+            System przekaże informacje do przedstawicieli wszystkich fabryk którzy do zakończenia przetargu będą w nim uczestniczyć w formie jak na giełdzie czyli każdy będzie informowany o
+            <br>  pojawieniu się nowej korzystniejszej oferty cenowo niż była.
+            Gwarantuje to najniższa możliwą cenę ponieważ nawet w przypadkach z nimi wieloletniej współpracy z naszego doświadczenia wiemy że i tak zawyżają wam cenę ponieważ brak konkurencji która zmusza do jej obniżenia.
+            <br>
+          </p>
+        </div>
+      </section>
+
+      <section class="py-20 px-4 bg-white">
         <div class="container mx-auto">
           <h2 class="text-4xl md:text-5xl font-bold mb-10 text-center">
             Zapoznaj się z tabelą cen od ponad 50 producentów!
           </h2>
           <iframe
               title="Tabelka cen styropianów"
-              data-src="https://admin.mega1000.pl/auctions/display-prices-table?zip-code=66-400"
+              src="https://admin.mega1000.pl/auctions/display-prices-table?zip-code=66-400"
               loading="lazy"
               style="height: 650px"
               class="w-full border-2 border-gray-200 rounded-lg shadow-lg"
@@ -95,27 +165,16 @@
         </div>
       </section>
 
-      <section class="py-20 px-4 bg-gray-100">
-        <div class="container mx-auto text-center">
-          <h2 class="text-4xl md:text-5xl font-bold mb-10">Jak Działa Nasz System Przetargowy</h2>
-          <p class="text-lg md:text-xl mb-10">
-            Proces jest prosty i przejrzysty – zacznij od dodania produktu do koszyka, a my zajmiemy się resztą, gwarantując najlepszą ofertę.
-          </p>
-          <!-- Detailed steps or an interactive guide could be added here -->
-        </div>
-      </section>
-
-      <section class="py-20 px-4 bg-gray-800 text-white">
+      <section class="py-20 px-4 bg-blue-600 text-white">
         <div class="container mx-auto">
           <h2 class="text-4xl md:text-5xl font-bold mb-10 text-center">Zarabiaj przez polecanie!</h2>
           <p class="text-lg md:text-xl mb-10">
             Mamy świetną wiadomość: zaproś do nas dowolną ilość znajomych, którzy jeszcze nie kupowali styropianów na naszej stronie, i odbierz 25 zł na każde zamówienie nowego użytkownika! Twoje bonusy automatycznie zamienią się na zniżki przy kolejnych zakupach. To proste!
           </p>
-          <a href="#" class="bg-blue-500 hover:bg-blue-600 text-white font-medium py-4 px-10 rounded-full inline-block transition-colors duration-300">Odkryj Oferty</a>
+          <a href="https://mega1000.pl/polec-znajomego" class="bg-blue-500 hover:bg-blue-700 text-white font-medium py-4 px-10 rounded-full inline-block transition-colors duration-300">Zobacz swój panel polecania</a>
         </div>
       </section>
-
-      <section class="py-20 px-4">
+      <section class="py-20 px-4 bg-gray-100">
         <div class="container mx-auto">
           <h2 class="text-4xl md:text-5xl font-bold mb-10 text-center">Skontaktuj się z Nami</h2>
           <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
@@ -131,23 +190,23 @@
         </div>
       </section>
 
-      <section class="py-20 px-4 bg-gray-100">
+      <section class="py-20 px-4 bg-white">
         <div class="container mx-auto">
           <h2 class="text-4xl md:text-5xl font-bold mb-10 text-center">Opinie Klientów</h2>
           <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
-            <div class="testimonial-card bg-white rounded-lg shadow-lg p-8">
+            <div class="testimonial-card bg-gray-100 rounded-lg shadow-lg p-8">
               <blockquote class="text-gray-700 italic mb-4">
                 "Jakość produktów i obsługa klienta są na najwyższym poziomie. Polecam każdemu, kto szuka niezawodnych rozwiązań izolacyjnych."
               </blockquote>
               <cite class="text-gray-600 font-bold">– Jan Kowalski</cite>
             </div>
-            <div class="testimonial-card bg-white rounded-lg shadow-lg p-8">
+            <div class="testimonial-card bg-gray-100 rounded-lg shadow-lg p-8">
               <blockquote class="text-gray-700 italic mb-4">
                 "Jakość produktów i obsługa klienta są na najwyższym poziomie. Polecam każdemu, kto szuka niezawodnych rozwiązań izolacyjnych."
               </blockquote>
               <cite class="text-gray-600 font-bold">– Jan Kowalski</cite>
             </div>
-            <div class="testimonial-card bg-white rounded-lg shadow-lg p-8">
+            <div class="testimonial-card bg-gray-100 rounded-lg shadow-lg p-8">
               <blockquote class="text-gray-700 italic mb-4">
                 "Jakość produktów i obsługa klienta są na najwyższym poziomie. Polecam każdemu, kto szuka niezawodnych rozwiązań izolacyjnych."
               </blockquote>
@@ -158,9 +217,9 @@
       </section>
     </main>
 
-    <footer class="footer py-6 bg-gray-800 text-white text-center">
-      <p>&copy; 2024 EPH Polska. Wszystkie prawa zastrzeżone.</p>
-      <p>Twój partner w zakresie efektywnych i ekonomicznych rozwiązań izolacyjnych.</p>
-    </footer>
+      <footer class="footer py-6 bg-gray-800 text-white text-center">
+        <p>&copy; 2024 EPH Polska. Wszystkie prawa zastrzeżone.</p>
+        <p>Twój partner w zakresie efektywnych i ekonomicznych rozwiązań izolacyjnych.</p>
+      </footer>
   </div>
 </template>
