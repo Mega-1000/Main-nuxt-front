adceffd7412a1cd721b6172592773e0f7d8cb3bc
Antoni Seba <antoniwoj@o2.pl>
2024-04-19 16:53:26 +0200

    Make recalc from csv work

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -8,47 +8,61 @@ const props = defineProps<{
 
 <template>
   <div
-    :class="`hidden lg:block w-15 md:w-25 h-fit w-full bg-white  ${
-      !nested ? `shadow-md border border-gray-500` : ``
+      :class="`lg:block w-15 md:w-25 h-fit w-full bg-white ${
+      !nested ? 'shadow rounded' : ''
     }`"
-    id="sidenavExample"
+      id="sidenavExample"
   >
     <h3
-      v-if="!nested"
-      class="text-xl lg:text-2xl py-5 uppercase px-3 font-medium bg-gray-100"
+        v-if="!nested"
+        class="text-xl lg:text-2xl py-4 px-6 font-semibold bg-gray-100 rounded-t-lg"
     >
       Kategorie
     </h3>
-    <ul>
+    <ul class="divide-y divide-gray-200">
       <li
-        id="sidenavEx2"
-        v-for="category in props.categories"
-        class="uppercase"
+          id="sidenavEx2"
+          v-for="category in props.categories"
+          class="uppercase"
       >
         <NuxtLink
-          :class="`py-3 lg:py-5 px-3 lg:px-5 ${nested && `pl-5 lg:pl-8`} flex border-t border-gray-500 items-center text-md lg:text-lg h-fit overflow-hidden text-gray-700 text-ellipsis ${categoryTree?.map((category: any) => category.id)[categoryTree.length - 1] === category.id && `bg-cyan-500 font-medium text-white`} hover:text-white hover:bg-cyan-500 transition duration-300 ease-in-out cursor-pointer`"
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="dark"
-          data-bs-toggle="collapse"
-          data-bs-target="#collapseSidenavEx2"
-          aria-expanded="false"
-          aria-controls="collapseSidenavEx2"
-          :href="`/${category.rewrite}/${category.id}`"
+            :class="`py-3 px-6 ${
+            nested && 'pl-8'
+          } flex items-center text-md lg:text-lg overflow-hidden text-gray-700 hover:text-white hover:bg-cyan-500 transition duration-300 ease-in-out cursor-pointer ${
+            categoryTree?.map((category: any) => category.id)[
+              categoryTree.length - 1
+            ] === category.id
+              ? 'bg-cyan-500 font-semibold text-white'
+              : ''
+          }`"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="dark"
+            :href="`/${category.rewrite}/${category.id}`"
         >
-          <p>
-            <span v-if="nested">
-            </span>
-            {{ category.name }}
-          </p>
+          <span v-if="nested" class="mr-2">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-4 w-4"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+            >
+              <path
+                  fill-rule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clip-rule="evenodd"
+              />
+            </svg>
+          </span>
+          {{ category.name }}
         </NuxtLink>
         <div
-          v-if="categoryTree?.map((category: any) => category.id).includes(category.id) && category.children"
+            v-if="categoryTree?.map((category: any) => category.id).includes(category.id) && category.children"
         >
           <Sidebar
-            :name="`${category.id}`"
-            :categories="category.children"
-            :category-tree="categoryTree"
-            nested
+              :name="`${category.id}`"
+              :categories="category.children"
+              :category-tree="categoryTree"
+              nested
           />
         </div>
       </li>
