119caa5e0ee72dcc8ee60cb9cfa36c3246e6198e
Antoni Seba <antoniwoj@o2.pl>
2024-04-21 10:55:57 +0200

    Make recalc from csv work

--- components/Footer.vue
+++ components/Footer.vue
@@ -8,14 +8,10 @@ onMounted(() => {
 </script>
 
 <template>
-  <footer
-    class="z-20 p-2 w-full bg-slate-100 border-t border-gray-200 shadow md:flex md:items-center md:justify-between"
-  >
-    <span class="text-xs md:text-sm text-gray-500 sm:text-center"
-      >EPH - ELEKTRONICZNA PLATFORMA HANDLOWA IWASZKIEWICZA 15A 55-200 OLAWA
-      NIP: 9121027907 Wszystkie prawa zastrzeżone.
+  <footer class="z-20 p-2 w-full bg-slate-100 border-t border-gray-200 shadow md:flex md:items-center md:justify-between">
+    <span class="text-xs md:text-sm text-gray-500 sm:text-center">
+      EPH - ELEKTRONICZNA PLATFORMA HANDLOWA IWASZKIEWICZA 15A 55-200 OLAWA NIP: 9121027907 Wszystkie prawa zastrzeżone.
     </span>
-
     <div @click="zipCodeChangeModalShown = true" v-if="zipCode && !zipCodeChangeModalShown" class="cursor-pointer">
       Zmień swój kod pocztowy, aktualnie: {{ zipCode }}
     </div>
