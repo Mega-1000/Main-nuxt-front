9d70bfb269bee2f4149e9d6240094d1464bf76fd
Antoni Seba <antoniwoj@o2.pl>
2024-04-25 09:18:32 +0200

    Make recalc from csv work

--- components/RecomendationSlider.vue
+++ components/RecomendationSlider.vue
@@ -10,7 +10,7 @@
               <img :src="`https://admin.mega1000.pl${product.url_for_website}`" :alt="product.name" class="h-48 w-full object-cover">
               <div class="px-4 py-3">
                 <h3 class="text-lg font-semibold text-gray-900">{{ product.name }}</h3>
-                <p class="text-gray-500">{{ getPriceString(product.meta_price, product.price) }} ZŁ</p>
+                <p class="text-gray-500">{{ getPriceString(product.meta_price, product.price) }}</p>
               </div>
             </nuxt-link>
           </div>
