397c89822dec3cd0bcd7ff11a36e40bac2208393
Antoni Seba <antoniwoj@o2.pl>
2024-04-26 13:21:26 +0200

    Make recalc from csv work

---
+++ pages/styropian-platnosc-za-pobraniem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mx-6 md:w-2/3 mx-auto">
+    SPOSOBY PLATNOŚCI ZA STYROPIAN
+    <p>
+      1.Płatność przy odbiorze
+      Polega ona na płatności zaliczki w wysokości np. 10 procent wartości oferty.
+      Następnie resztę płatności dokonuje się przy dostawie błyskawicznym przelewem na konto tak aby dotarło ono do 15 minut od przyjazdu kierowcy.
+      Prosimy pamiętać aby zarezerwować sobie możliwość awaryjnej zapłaty w razie losowego zdarzenia np. brak sieci internetowej itp.
+      Po otrzymaniu wpływu na nasze konto kierowca zostanie powiadomiony o możliwości wydania towaru.
+      W przypadku gdyby dopłata nie pojawiła się na naszym koncie to kierowca będzie zmuszony towar zawieźć z powrotem na magazyn wydający a koszty z tym związane obciążą Państwa konto.
+      Dodatkowy koszt związany z taką obsługą to 49 zł
+    </p>
+
+    <p class="mx-4">
+      2.Wpłata 100 zł jako zaliczki i resztę po potwierdzeniu przez fabrykę o dokonaniu dla Państwa awizacji.
+      Dodatkowy koszt związany z taką obsługą to 49 zł
+    </p>
+
+    <p>
+      3.Wpłata całości przed dostawą.
+      Brak dodatkowego kosztu.
+    </p>
+  </div>
+</template>
