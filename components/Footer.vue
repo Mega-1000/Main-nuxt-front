b684ada34efef85f6afc05556de686c98004cd0f
Antoni Seba <antoniwoj@o2.pl>
2024-04-26 15:21:35 +0200

    Make recalc from csv work

--- components/Footer.vue
+++ components/Footer.vue
@@ -10,7 +10,7 @@ onMounted(() => {
 <template>
   <footer class="z-20 mt-10 p-2 w-full bg-slate-100 border-t border-gray-200 shadow md:flex md:items-center md:justify-between">
     <span class="text-xs md:text-sm text-gray-500 sm:text-center">
-      EPH - ELEKTRONICZNA PLATFORMA HANDLOWA IWASZKIEWICZA 15A 55-200 OLAWA NIP: 9121027907 Wszystkie prawa zastrzeżone.
+      EPH - ELEKTRONICZNA PLATFORMA HANDLOWA POLSKA SPÓŁKA Z OGRANICZONĄ ODPOWIEDZIALNOŚCIĄ SP. z o. o. 15A 55-200 OLAWA NIP: 8971930266 Wszystkie prawa zastrzeżone.
     </span>
     <div @click="zipCodeChangeModalShown = true" v-if="zipCode && !zipCodeChangeModalShown" class="cursor-pointer">
       Zmień swój kod pocztowy, aktualnie: {{ zipCode }}
