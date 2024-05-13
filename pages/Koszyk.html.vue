343a6a5f23f1b018f5afd413a4d267faaabcbb6e
Antoni Seba <antoniwoj@o2.pl>
2024-05-13 07:11:30 +0200

    Make recalc from csv work

--- pages/Koszyk.html.vue
+++ pages/Koszyk.html.vue
@@ -645,12 +645,12 @@ const nextDayAt24PM = computed(() => {
 
               <div>
                 <label for="delivery-start-date" class="block mb-2 text-sm font-medium text-gray-900">od</label>
-                <input type="datetime-local" id="delivery-start-date" v-model="deliveryStartDate" class="block w-full px-3 py-2 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+                <input type="date" id="delivery-start-date" v-model="deliveryStartDate" class="block w-full px-3 py-2 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
               </div>
 
               <div class="mt-4">
                 <label for="delivery-end-date" class="block mb-2 text-sm font-medium text-gray-900">do</label>
-                <input type="datetime-local" id="delivery-end-date" v-model="deliveryEndDate" class="block w-full px-3 py-2 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+                <input type="date" id="delivery-end-date" v-model="deliveryEndDate" class="block w-full px-3 py-2 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
               </div>
 
             </div>
