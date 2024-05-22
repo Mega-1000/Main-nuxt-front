379fedbb0044fa476d17801b785957b356580401
Antoni Seba <antoniwoj@o2.pl>
2024-05-22 07:22:42 +0200

    Make recalc from csv work

--- pages/[productRewrite]/[productId]/index.vue
+++ pages/[productRewrite]/[productId]/index.vue
@@ -128,7 +128,7 @@ onMounted(async () => {
     isMainStyrofoamLobby.value = true;
 
     const c = await shopApi.get(`/api/get-blurred-categories/101?zip-code=${localStorage.getItem('zipCode')}`);
-    currentProduct.value?.currentProduct.children = c.data
+    // currentProduct.value?.currentProduct.children = c.data
   }
 
 
