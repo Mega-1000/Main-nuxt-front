2e25f352bb85e915215a928a366a2b4a4a5ef80f
Antoni Seba <antoniwoj@o2.pl>
2024-05-29 13:34:40 +0200

    Make recalc from csv work

--- pages/styropiany.vue
+++ pages/styropiany.vue
@@ -58,11 +58,9 @@ const onIframeError = () => {
             <Loader :showLoader="isLoading" />
           </div>
           <iframe
-              v-show="!isLoading"
               ref="priceTable"
               title="Tabelka cen styropianów"
               :src="iframeSrc"
-              loading="lazy"
               :style="isLoading ? 'height: 800px' : 'height: 1px'"
               class="w-full border-2 border-gray-200 rounded-lg shadow-lg"
               sandbox="allow-scripts allow-same-origin"
