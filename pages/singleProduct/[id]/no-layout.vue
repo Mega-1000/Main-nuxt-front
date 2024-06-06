2f877c4079b5aa1746038791817fd23025931319
Antoni Seba <antoniwoj@o2.pl>
2024-06-06 08:14:13 +0200

    Make recalc from csv work

--- pages/singleProduct/[id]/no-layout.vue
+++ pages/singleProduct/[id]/no-layout.vue
@@ -34,8 +34,8 @@ onMounted(async () => {
   // Check if the page should reload and if it has not already been reloaded
   if (item.value.variation_group !== 'styropiany' && route.query.reload !== '1') {
     // Redirect with a reload query parameter set to 1
-    // const newUrl = `${window.location.pathname}${window.location.search ? window.location.search + '&' : '?'}reload=1`;
-    // window.location.href = newUrl;
+    const newUrl = `${window.location.pathname}${window.location.search ? window.location.search + '&' : '?'}reload=1`;
+    window.location.href = newUrl;
   }
 });
 
