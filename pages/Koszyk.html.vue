f31e757563d79d53974afffc475cfcea31f1e089
Antoni Seba <antoniwoj@o2.pl>
2024-06-10 15:03:11 +0200

    Make recalc from csv work

--- pages/Koszyk.html.vue
+++ pages/Koszyk.html.vue
@@ -140,6 +140,18 @@ onMounted(async () => {
   window.dispatchEvent(new CustomEvent('token-refreshed'));
 
   productsCart.value = cart;
+
+  const startDatePicker = useDatePicker('#delivery-start-date', {
+    altInput: true,
+    altFormat: 'F j, Y',
+    dateFormat: 'Y-m-d',
+  });
+
+  const endDatePicker = useDatePicker('#delivery-end-date', {
+    altInput: true,
+    altFormat: 'F j, Y',
+    dateFormat: 'Y-m-d',
+  });
 });
 
 const handleDelete = async () => {
@@ -603,12 +615,12 @@ const ShipmentCostItemsLeftText = (product: any) => {
 
               <div>
                 <label for="delivery-start-date" class="block mb-2 text-sm font-medium text-gray-900">od</label>
-                <input type="date" id="delivery-start-date" v-model="deliveryStartDate" class="block w-full px-3 py-2 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" required>
+                <input type="text" id="delivery-start-date" v-model="deliveryStartDate" class="block w-full px-3 py-2 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" required>
               </div>
 
               <div class="mt-4">
                 <label for="delivery-end-date" class="block mb-2 text-sm font-medium text-gray-900">do</label>
-                <input type="date" id="delivery-end-date" v-model="deliveryEndDate" class="block w-full px-3 py-2 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" required>
+                <input type="text" id="delivery-end-date" v-model="deliveryEndDate" class="block w-full px-3 py-2 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" required>
               </div>
 
             </div>
