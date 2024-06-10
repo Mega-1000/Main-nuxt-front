7d69d699831cd312abb8ab6baa9e01b960a674a2
Antoni Seba <antoniwoj@o2.pl>
2024-06-10 20:33:28 +0200

    Make recalc from csv work

---
+++ pages/styro-helper.vue
@@ -0,0 +1,27 @@
+<template>
+  <input type="text" v-model="prompt">
+  <button @click="sendS">
+    send
+  </button>
+
+  <div v-html="form">
+
+  </div>
+
+
+</template>
+
+<script setup>
+  import axios from "axios";
+
+  const prompt = ref('');
+  const form = ref('')
+
+  const sendS = () => {
+    axios.post('https://admin.mega1000.pl/api/styro-help', {
+      'message': prompt.value
+    }).then((res) => {
+      form.value = res.data
+    })
+  }
+</script>
