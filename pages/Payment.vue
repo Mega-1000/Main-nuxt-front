db31e56e60ca267e2fe9cc5a4fa8a85d0f417531
Antoni Seba <antoniwoj@o2.pl>
2024-06-12 14:11:27 +0200

    Make recalc from csv work

--- pages/Payment.vue
+++ pages/Payment.vue
@@ -55,6 +55,23 @@ const handleClick = () => {
           !!! UWAGA od dnia 2023.12.13 zmiana numeru konta oraz firmy !!!
       </div>
 
+      <div class="my-4">
+        UWAGA! Dane do wysyłki nie zostały uzupełnione.
+        <button
+            v-if="paymentData?.id"
+            class="w-30 text-white bg-cyan-400 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            @click="handleClick"
+        >
+          Uzupełnij dane do przesyłki
+        </button>
+      </div>
+
+      <div class="my-4">
+        <a :href="`https://mega1000.pl/account?attachtransferconfirmation=${paymentData?.id}`" class="w-30 text-white bg-cyan-400 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+          Podłącz potwierdzenie przelewu
+        </a>
+      </div>
+
       <p class="text-md md:text-xl">
         Jeśli potrzebujesz się skontaktować zaloguj się i wejdź w zakładkę "Moje
         Konto" (domyślnie nr telefonu jest hasłem)
@@ -98,13 +115,6 @@ const handleClick = () => {
           />
         </div>
       </div>
-      <button
-        v-if="paymentData?.id"
-        class="w-30 text-white bg-cyan-400 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-        @click="handleClick"
-      >
-        Uzupełnij dane do przesyłki
-      </button>
     </div>
   </div>
 </template>
