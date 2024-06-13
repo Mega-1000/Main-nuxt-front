a4d630517035d5ac89a66386a5b238cecf3f350c
Antoni Seba <antoniwoj@o2.pl>
2024-06-13 18:23:15 +0200

    Make recalc from csv work

--- pages/singleProduct/[id].vue
+++ pages/singleProduct/[id].vue
@@ -99,7 +99,7 @@ const goBack = () => {
     <div
         id="calculatorModal"
         tabindex="-1"
-        class="top-0 fixed z-50 w-auto hidden md:p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal w-[100%] md:w-[65%] md:h-full"
+        class="top-0 fixed z-50 w-auto hidden md:p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal w-[100%] md:h-full"
     >
       <div
           class="relative w-full h-full max-w-xl sm:max-w-3xl md:max-w-5xl lg:max-w-7xl md:h-auto"
