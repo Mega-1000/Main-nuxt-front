936056e406d65eac4af341e651f2f330dd62c7ac
Antoni Seba <antoniwoj@o2.pl>
2024-06-15 10:24:34 +0200

    Make recalc from csv work

--- pages/Koszyk.html.vue
+++ pages/Koszyk.html.vue
@@ -653,7 +653,7 @@ const ShipmentCostItemsLeftText = (product: any) => {
             </div>
 
             <p class="mt-2 text-sm text-red-600">{{ errorText2 }}</p>
-            <SubmitButton :disabled="loading" type="submit">Wyślij zapytanie ofertowe</SubmitButton>
+            <SubmitButton :disabled="loading" type="submit">Wyślij zapytanie - do niczego nie zobowiązuje</SubmitButton>
 
             <div class="text-red-600 font-bold" v-if="isOrderSmall">
               Z powodu że aktualne produkty z koszyka nie przekraczają 10m3 nie jest możliwa dostawa.
