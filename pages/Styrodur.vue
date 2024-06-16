5b44a4cfeaf98c8d15c0b0f7ec4b0b68c7c0308b
Antoni Seba <antoniwoj@o2.pl>
2024-06-16 20:50:55 +0200

    Make recalc from csv work

--- pages/Styrodur.vue
+++ pages/Styrodur.vue
@@ -1,3 +1,3 @@
 <script setup>
-  window.location.href = 'https://mega1000.pl/----20styropiany-xps-styrodury/6'
+  window.location.href = 'https://mega1000.pl/20styropiany-xps-styrodury/7'
 </script>
