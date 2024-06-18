cee3f65486800b6c27bb221dbd1f8620fb94a738
Antoni Seba <antoniwoj@o2.pl>
2024-06-18 09:57:46 +0200

    Make recalc from csv work

--- pages/styropiany.vue
+++ pages/styropiany.vue
@@ -157,6 +157,20 @@ const playVideo  = () => {
             </div>
           </nuxt-link>
         </div>
+
+        <section class="py-10 px-2 md:py-20 md:px-4 bg-gray-100">
+          <div class="container mx-auto">
+            <h2 class="text-2xl md:text-4xl md:text-5xl font-bold mb-4 md:mb-10 text-center">Skontaktuj się z nami</h2>
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
+              <div class="contact-card bg-white rounded-lg shadow-lg p-4 md:p-8">
+                <h3 class="text-xl md:text-2xl font-bold mb-2 md:mb-4">Infolinia</h3>
+                <p class="text-gray-600">Telefon: <span class="font-bold">+48 576 205 389</span></p>
+                <p class="text-gray-600">Godziny pracy: <span class="font-bold">6:30 - 22:00</span></p>
+              </div>
+            </div>
+          </div>
+        </section>
+
         <a class="text-center mt-8 text-lg md:text-xl w-fit mx-auto flex gap-2 align-center" href="#price-table">
           <div>
             Zobacz tabelę cen
