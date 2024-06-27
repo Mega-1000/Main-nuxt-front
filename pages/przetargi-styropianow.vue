f3b04fed338cb248bbdde276365ab40d05c31c1a
Antoni Seba <antoniwoj@o2.pl>
2024-06-27 16:44:21 +0200

    Make recalc from csv work

--- pages/przetargi-styropianow.vue
+++ pages/przetargi-styropianow.vue
@@ -258,7 +258,7 @@ onMounted(async () => {
   try {
     loading.value = true;
     const types = await shopApi.get('/auctions/get-styrofoam-types');
-    styrofoamTypes.value = types.value;
+    styrofoamTypes.value = types.data;
   } finally {
     loading.value = false;
   }
