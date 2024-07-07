1cd609b344e011fc7eda45062cd0b1f0cda38f33
Antoni Seba <antoniwoj@o2.pl>
2024-07-07 07:35:08 +0200

    Make recalc from csv work

--- pages/firms/Przetargi.vue
+++ pages/firms/Przetargi.vue
@@ -95,7 +95,7 @@ const formatDate = (dateString: string) => {
                   Data dostawy
                 </dt>
                 <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                  {{ formatDate(auction.date_of_delivery) }}
+                  {{ auction.date_of_delivery }}
                 </dd>
               </div>
               <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
